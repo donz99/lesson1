5420b5b0a7198aab45ea18f8ad6536af1f7aa45b
donz99 <35003466+donz99@users.noreply.github.com>
2022-06-28 20:49:08 +0300

    Lesson1

---
+++ Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Begin1. Дана сторона квадрата a. Найти его периметр P = 4·a
+        //Ввод или присвоение исходных данных
+        /*int a = 5;
+        //Вычисление результата
+        int P = 4 * a;
+        //Вывод результата
+        System.out.println(P);
+        */
+        // Дана сторона квадрата a. Найти его площадь S = a**2
+        /*int a = 8;
+        int S = a * a;
+        System.out.println("У квадрат со стороной " + a + " площадь " + S);
+        */
+        //Begin4 Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
+        //значения π использовать 3.14.
+        /*int d = 10;
+        final double pi = 3.14;
+        double L = pi*d;
+        System.out.println("У круга с диаметром " + d + " длина окружности " + L);
+
+         */
+        //Begin 5 Дана длина ребра куба a. Найти объем куба V = a**3 и площадь его поверхности S = 6·a**2
+        /*int a = 5;
+        int V = a * a * a;
+        int S = 6 * a * a;
+        System.out.println(V);
+        System.out.println(S);
+
+         */
+
+        //Begin 6  Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти его объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
+        /*int a = 5;
+        int b = 7;
+        int c = 9;
+        int S = 2 * (a*b+b*c+a*c);
+        System.out.println(S);
+
+         */
+
+        //Begin 7 Найти длину окружности L и площадь круга S заданного радиуса R: L = 2·π·R, S = π·R**2
+        /*final double pi = 3.14;
+        int R = 5;
+        double L = 2 * pi * R;
+        double S = pi * R * R;
+        System.out.println(L);
+        System.out.println(S);
+
+         */
+
+        //Begin 8 Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
+        /*int a = 2;
+        double b = 3;
+        double x = (double)(a+b)/2;
+        System.out.println(x);
+
+         */
+
+
+        //Begin 9 Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то есть квадратный корень из их произведения: √a·b
+        int a = 5;
+        int b = 2;
+        int x = math.sqr(a * b);
+        System.out.println(x);
+
+
+        //2
+        //.
+
+    }
+}
